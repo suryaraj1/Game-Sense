4b3f77fd845840d2d2a6ade069af83db34a0fc4c
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-01 18:19:41 +0530

    Clear factory code and add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header-wrapper'>
+            <div className='header-text-container'>
+                <p className='header-text'>Game Sense</p>
+            </div>
+        </div>  
+    );
+}
+
+export default Header;
