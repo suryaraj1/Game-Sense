50f28961e620d2fe60149f31cacb830b97e6c807
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-02 15:55:15 +0530

    Change Footer position

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <div className='footer-container'>
+        <div className='footer-wrapper'>
             <p className='footer'>Made with 💗 by Suryaraj</p>
         </div>
     )
