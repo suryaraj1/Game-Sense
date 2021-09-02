d63455292729280360c3878e3b2a9785aea61a40
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-02 15:47:49 +0530

    Add Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer-container'>
+            <p className='footer'>Made with 💗 by Suryaraj</p>
+        </div>
+    )
+}
+
+export default Footer;
